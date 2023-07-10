403d842f5df5aa66dd2778b9a2c6088b544886b4
Kayode Ademolaji <kayode@rb2.ng>
2023-07-10 05:14:44 +0100

    added global exception handler and logger

---
+++ PlateaumedPro.Common/Exceptions/PlateaumedProExceptionMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace PlateaumedPro.Common
+{
+    public class PlateaumedProExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerService _loggerService;
+
+        public PlateaumedProExceptionMiddleware(RequestDelegate next, ILoggerService loggerService)
+        {
+            _next = next;
+            _loggerService = loggerService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                await LogExceptionAsync(context, ex).ConfigureAwait(false);
+            }
+        }
+
+        private Task LogExceptionAsync(HttpContext context, Exception exception)
+        {
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            var result = JsonConvert.SerializeObject(new
+            {
+                Status = statusCode,
+                Error = exception.Message
+            });
+
+            var request = context.Request;
+            _loggerService.Error($"[PlateaumedProGlobalException] Request Path: [{request?.Path}], Error => [{exception.Message}], StackTrace => [{exception.StackTrace}]");
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ PlateaumedPro.Common/Helpers/LoggerService.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+
+namespace PlateaumedPro.Common
+{
+    public class LoggerService : ILoggerService
+    {
+        private static NLog.ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public void Error(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void Info(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Debug(string message)
+        {
+            logger.Debug(message);
+        }
+
+
+        public void Warn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
+
